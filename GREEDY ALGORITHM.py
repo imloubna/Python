42088a9b19c2d08a00284b2082b1ccfeed276c36
imloubna <mloubnaimmame@gmail.com>
2026-03-14 16:14:02 +0530

    Add files via upload

---
+++ GREEDY ALGORITHM.py
@@ -0,0 +1,15 @@
+x=int(input("Enter the amount:"))
+coins=[500,200,100,50,20,10,2,1]
+for i in coins:
+    while x >= i:
+        print(i,end=" ")
+        x=x-i
+        
+        
+        
+        
+        
+        
+        
+        
+        
