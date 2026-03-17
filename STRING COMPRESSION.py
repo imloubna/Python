be23c129c8a6eb06034151a5d3039ce4e0783200
imloubna <mloubnaimmame@gmail.com>
2026-03-17 16:03:31 +0530

    Add files via upload

---
+++ STRING COMPRESSION.py
@@ -0,0 +1,12 @@
+a= input("Enter the string: ")
+res = ""
+c=1
+for i in range(len(a)):
+    if (i+1<len(a) and a[i]==a[i+1]):
+        c=c+1
+    else:
+        res=res+a[i]
+        res=res+str(c)
+        c=1
+print(res)        
+    
